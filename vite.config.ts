e76a5aed3e3071ecf48886e1663a06aff146fa50
Test <nr14082005@gmail.com>
2026-06-01 00:32:47 +0530

    enable spa prerender

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,14 @@ const config = defineConfig({
       projects: ['./tsconfig.json'],
     }),
     tailwindcss(),
-    tanstackStart(),
+    tanstackStart({
+      spa: {
+        enabled: true,
+        prerender: {
+          outputPath: '/',
+        },
+      },
+    }),
     viteReact(),
   ],
 })
